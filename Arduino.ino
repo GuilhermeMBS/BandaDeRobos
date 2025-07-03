3f9ff3305913683b7d8557f2b318f4ce668b5f0f
Netec120 <78117350+Netec120@users.noreply.github.com>
2025-07-03 19:14:00 -0300

    Create Arduino.ino

---
+++ Arduino.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.setTimeout(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()>0){
+    String texto = Serial.readStringUntil('\n');
+    Serial.println(texto + " copiado");
+  }
+}
